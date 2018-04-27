a57a740b43db4d682613f937daa2f360ff2ec919
Masanori Funaki <funakimfunaki@gmail.com>
2018-04-27 10:15:45 +0000

    追加

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@ $(function ($) {
     'use strict';
 
     $('a[target="_blank"]').css('color', 'red');
-    // 全一致
+    // 部分一致
     $('input[name*="cat"').css('height', '100px');
-
+    // 完全一致
+    $('input[name~="cat"').css('height', '50px');
+    // 後方一致
+    $('a[href$="masanorifunaki"').css('color', 'black');
+    // 子セレクター
+    $('body > h2').css('color', 'blue');
+    // 隣接兄弟セレクター
+    $('h3 + p').fadeOut();
+    // 一般兄弟セレクター
+    $('h4 ~ p').fadeOut();
 });
